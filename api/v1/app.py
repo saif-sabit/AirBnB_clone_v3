38530875de84e9bd934bbb3b0ab8d5865f5c654a
saif-sabit <saifsabit@gmail.com>
2023-10-30 19:57:47 +0300

    Task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+"""
+app file 
+"""
 from flask import Flask
-app = Flask()
-form models import storage
+from models import storage
+from api.v1.views import app_views
 
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """
+    Tears down .
+    """
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, threaded=True)
+ 
