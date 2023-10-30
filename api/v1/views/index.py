fbd315612a0f649850975df3dfd32a9f6f4bf4fb
saif-sabit <saifsabit@gmail.com>
2023-10-31 01:16:36 +0300

    Task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,13 +4,12 @@ Route for status
 """
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def api_status():
     """
-        return Ok
+    return Ok
     """
-
-    return (jsonify({'status': 'OK'}))
+    response = {"status": "OK"}
+    return jsonify(response)
