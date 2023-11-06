370e4292982ba62abfa3b44cb6704c87989af267
saif-sabit <saifsabit@gmail.com>
2023-11-06 20:04:45 +0300

    update

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,19 +8,23 @@ from models import storage
 from models.place import Place
 
 
-@app_views.route('/places', methods=['GET'])
+@app_views.route('/cities/<city_id>/places',
+                 methods=['GET'], strict_slashes=False)
 def api_places_list():
     """
     return Ok
     """
-    places = storage.all(Place).values()
-    response = []
-    for place in places:
-        response.add(place.to_dict())
-    return jsonify(response)
+    city = storage.get(City, city_id)
+    if city:
+        places = storage.all(Place).values()
+        city_places = [place.to_dict(
+            ) for place in places if place.city_id == city_id]
+        return jsonify(city_places)
+    abort(404)
 
 
-@app_views.route('/places/<place_id>', methods=['GET'])
+@app_views.route('/places/<place_id>',
+                 methods=['GET'], strict_slashes=False)
 def api_places(place_id):
     """
     return place
@@ -32,7 +36,8 @@ def api_places(place_id):
     return jsonify(response)
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'])
+@app_views.route('/places/<place_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def api_places_delete(place_id):
     """
     return place
@@ -41,28 +46,51 @@ def api_places_delete(place_id):
     if place is None:
         abort(404)
     storage.delete(place)
-    storage.save(place)
-    response = {}, 200
-    return jsonify(response)
+    storage.save()
+    return jsonify({})
 
 
-@app_views.route('/places', methods=['POST'])
-def api_places_add(place_id):
+@app_views.route('/cities/<city_id>/places',
+                 methods=['POST'], strict_slashes=False)
+def api_places_add(city_id):
     """
     return add place
     """
 
-    response = {}, 200
-    return jsonify(response)
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Not a JSON"}), 400
+    if 'user_id' not in data:
+        return jsonify({"error": "Missing user_id"}), 400
+    user = storage.get(User, data['user_id'])
+    if not user:
+        abort(404)
+    if 'name' not in data:
+        return jsonify({"error": "Missing name"}), 400
+    data['city_id'] = city_id
+    new_place = Place(**data)
+    new_place.save()
+    return jsonify(new_place.to_dict()), 201
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'])
+@app_views.route('/places/<place_id>',
+                 methods=['PUT'], strict_slashes=False)
 def api_places_update(place_id):
     """
     return update place
     """
     place = storage.get(Place, place_id)
-    if place is None:
-        abort(404)
-    response = {}, 200
-    return jsonify(response)
+    if place:
+        data = request.get_json()
+        if not data:
+            return jsonify({"error": "Not a JSON"}), 400
+        ignore_keys = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
+        for key, value in data.items():
+            if key not in ignore_keys:
+                setattr(place, key, value)
+        place.save()
+        return jsonify(place.to_dict())
+    abort(404)
